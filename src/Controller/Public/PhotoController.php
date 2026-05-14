66a17f9e8896746c4e2c9f0d78e91678a01aec0c
rozenrade <boyluffi@gmail.com>
2026-05-14 14:08:45 +0200

    adjustment of userchecker

---
+++ src/Controller/Public/PhotoController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Public;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PhotoController extends AbstractController
+{
+    #[Route('/photo/', name: 'app_photo')]
+    public function index(): Response
+    {
+        return $this->render('photo/index.html.twig', [
+            'controller_name' => 'PhotoController',
+        ]);
+    }
+}
